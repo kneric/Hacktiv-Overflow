b903f9edc1b4179de7fcc966726b49e21d42703a
kneric <brian.pradipta.a@gmail.com>
2018-08-28 11:36:22 +0700

    setting up main.js

--- overflow/src/main.js
+++ overflow/src/main.js
@@ -3,6 +3,29 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import swal from 'sweetalert'
+import wysiwyg from 'vue-wysiwyg'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faAngry, faGrinBeam} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+library.add(faAngry, faGrinBeam)
+Vue.component('fa-icon', FontAwesomeIcon)
+
+Vue.use(wysiwyg, {
+  hideModules: {
+    "image": true
+  }
+}); 
+
+Vue.prototype.$axios = axios.create(
+  {
+    baseURL: 'http://35.240.195.13'
+  }
+)
+Vue.prototype.$swal = swal;
+Vue.prototype.$wysiwyg = wysiwyg;
 
 Vue.config.productionTip = false
 
